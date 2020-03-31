b06b6544a1c3926a2e2d4e01b0fb575be3c35a42
1113862469@qq.com <1113862469@qq.com>
2020-03-31 09:41:22 +0800

    更正, 我那写的还不是冒泡排序, 我也不知道是什么排序; 更新真正的冒泡排序

--- lt_20202_3_31/shu_zu_pai_xu.py
+++ lt_20202_3_31/shu_zu_pai_xu.py
@@ -3,6 +3,7 @@ from typing import List
 
 class Solution:
     """冒泡排序, 时间超出"""
+
     def sortArray(self, nums: List[int]) -> List[int]:
         for i in range(len(nums)):
             # min_num = nums[i]
@@ -12,5 +13,26 @@ class Solution:
 
         return nums
 
+    def maopao(self, a):
+        """
+        冒泡排序
 
-Solution().sortArray([5, 1, 1, 2, 0, 0])
+        1. 比较相邻的元素。如果第一个比第二个大，就交换它们两个；
+        2. 对每一对相邻元素作同样的工作，从开始第一对到结尾的最后一对，这样在最后的元素应该会是最大的数；
+        3. 针对所有的元素重复以上的步骤，除了最后一个；
+        4. 重复步骤1~3，直到排序完成。
+        todo: 此冒泡排序还可以优化
+        :param a:
+        :return:
+        """
+        length = len(a)
+        for i in range(length):
+            for j in range(1, length - i):
+                if a[j - 1] > a[j]:
+                    a[j], a[j - 1] = a[j - 1], a[j]
+        print(a)
+
+        return a
+
+
+Solution().maopao([5, 1, 1, 2, 0, 0])
