dcfa146b717486a11395c8377ad10d40ec5ae23b
jax <1113862469@qq.com>
2022-05-29 10:16:40 +0800

    链表遍历

---
+++ lt_2022_5_29/main.py
@@ -0,0 +1,46 @@
+"""
+# 06. 从尾到头打印链表
+
+输入一个链表的头节点，从尾到头反过来返回每个节点的值（用数组返回）。
+
+ 
+
+示例 1：
+
+输入：head = [1,3,2]
+输出：[2,3,1]
+ 
+
+限制：
+
+0 <= 链表长度 <= 10000
+
+来源：力扣（LeetCode）
+链接：https://leetcode.cn/problems/cong-wei-dao-tou-da-yin-lian-biao-lcof
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+"""
+
+
+# Definition for singly-linked list.
+from typing import List
+
+
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+class Solution:
+    def reversePrint(self, head: ListNode) -> List[int]:
+        if not head:
+            return []
+        if not head.next:
+            return [head.val]
+
+        from collections import deque as d
+        ret = d()
+        current = head
+        while current:
+            ret.appendleft(current.val)
+            current = current.next
+        return list(ret)
